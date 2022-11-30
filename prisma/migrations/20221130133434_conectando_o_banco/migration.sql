12097138603c548edf1711bfdd5a5b4352ddb711
llanunes <22102365@dsn127m1242315.cfp127aluno.local>
2022-11-30 11:28:48 -0300

    em andamento

---
+++ prisma/migrations/20221130133434_conectando_o_banco/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `teste` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `teste_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/app.js
+++ src/app.js
@@ -22,10 +22,37 @@
 
 import express from 'express';
 import cors from 'cors';
+import { MESSAGE_ERROR, MESSAGE_SUCESS } from './modulo/config.js';
+
+import controllerProduto from './controller/controllerProduto.js';
 
 const app = express();
 
 app.use(express.json(), cors());
+
+// EndPoint para listar todos os produtos 
+app.get('/v1/produtos', cors(), async (request, response) => {
+
+  let message;
+  let statusCode;
+  
+  // Retorna todos os alunos existentes do BD
+  const dadosProduto = await controllerProduto.listarProdutos();
+  
+  // valida se existe retorno 
+  if(dadosProduto){
+      statusCode = 200;
+      console.log(dadosProduto);
+      message = dadosProduto;
+  } else {
+      statusCode = 404; 
+      message = MESSAGE_ERROR.NOT_FOUND_BD;
+  }
+
+  response.status(statusCode);
+  response.json(message);
+});
+
 
 app.listen(8080, () => {
   console.log('Server listening at port 8080...');
